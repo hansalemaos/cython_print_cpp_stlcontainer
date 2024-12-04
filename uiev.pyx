0c0d374e7307e2c8e5f68559dbe4a3c0c7aec276
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2024-12-03 23:38:44 -0300

    update

--- uiev.pyx
+++ uiev.pyx
@@ -1,12 +1,3 @@
-# Print any nested C++ STL container in Cython/Python
-
-### Download the C++ header file here:
-
-https://github.com/hansalemaos/stl_pretty_print/blob/main/prettyprint.hpp
-
-```py 
-
-# some examples of the data types you can print 
 from libcpp.vector cimport vector
 from libcpp.string cimport string 
 from libcpp.set cimport multiset,set,pair 
@@ -16,9 +7,6 @@ from libcpp.deque cimport deque
 from libcpp.queue cimport queue,priority_queue
 from libcpp.unordered_set cimport unordered_set, unordered_multiset
 from libcpp.forward_list cimport forward_list
-
-# use this code to include the STL printer 
-# https://github.com/hansalemaos/stl_pretty_print/blob/main/prettyprint.hpp
 cdef extern from "prettyprint.hpp" namespace "prettyprinter"  nogil :
     string print_black[T](T, string, string)
     string print_red[T](T, string, string)
@@ -45,7 +33,6 @@ cdef extern from "prettyprint.hpp" namespace "prettyprinter"  nogil :
     string print_cyan[T](T)
     string print_white[T](T)
 
-# how to use it 
 cdef:
     vector[int] hallo = [1,2,3,4,5]
     unordered_map[int,int] hallo2={3:4,2:3,1:12}
@@ -53,4 +40,3 @@ cdef:
     string tete2=print_red(hallo2)
     string tete3=print_yellow(hallo,<string>b"debug_prefix")
     string tete4=print_cyan(hallo2,<string>b"debug_prefix",<string>b"debug_suffix")
-```
